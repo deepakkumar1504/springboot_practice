203efc97b0ead2c793b831476e8e4843f18e983c
Deepak Kumar <deepakkumar@Deepaks-MacBook-Pro.local>
2025-04-10 12:03:26 +0530

    modified pom.xml

--- src/main/java/com/mycompany/springboot_practice/SpringbootPracticeApplication.java
+++ src/main/java/com/mycompany/springboot_practice/SpringbootPracticeApplication.java
@@ -1,13 +1,19 @@
 package com.mycompany.springboot_practice;
 
+import javax.sql.DataSource;
+
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 public class SpringbootPracticeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootPracticeApplication.class, args);
+		
 	}
 
 }
---
+++ src/main/java/com/mycompany/springboot_practice/controller/Container.java
@@ -0,0 +1,8 @@
+package com.mycompany.springboot_practice.controller;
+
+import org.springframework.context.ApplicationContext;
+
+public class Container {
+
+	
+}
